c8e7f4f5c6f720cacfcea5cec4568c48ab5f85ed
federico <federicoadegiovanni@gmail.com>
2021-11-01 22:37:05 -0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Phaser from 'phaser'
 import preloader from './scenes/preloader'
 import tutorial from './scenes/tutorial'
 import mainmenu from './scenes/mainmenu'
-import creditos	 from './scenes/creditos'
+import creditos from './scenes/creditos'
 import informacion from './scenes/informacion'
 import game from './scenes/game'
 import UIscene from './scenes/UIscene'
@@ -13,20 +13,20 @@ import menuingame from './scenes/menuingame'
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
 	scale: {
-        mode: Phaser.Scale.FIT,
-        parent: '',
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 1920,
-        height: 1080
-    },
+		mode: Phaser.Scale.FIT,
+		parent: '',
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+		width: 1920,
+		height: 1080
+	},
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 0 }
-			, debug: false
+			gravity: { y: 0 },
+			debug: true,
 		}
 	},
-	scene: [preloader,mainmenu, tutorial, informacion, creditos, game,UIscene, menuingame]
+	scene: [preloader, mainmenu, tutorial, informacion, creditos, game, UIscene, menuingame]
 }
 
 export default new Phaser.Game(config)
